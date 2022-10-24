c814c802a69bde1ab06a2db8d7febf467d7fee27
yyokii <yyokii.h@gmail.com>
2022-10-24 22:39:21 +0900

    ♻️  Refactor run.ts

--- grit-nfts/scripts/run.ts
+++ grit-nfts/scripts/run.ts
@@ -1,11 +1,19 @@
-const main = async () => {
-  const nftContractFactory = await hre.ethers.getContractFactory('GritNFT')
+async function main() {
+  const nftContract = await deploy('GritNFT')
+
+  let txn = await nftContract.makeNFT()
+  await txn.wait()
+}
+
+async function deploy(name: String): Promise<any> {
+  const nftContractFactory = await hre.ethers.getContractFactory(name)
   const nftContract = await nftContractFactory.deploy()
   await nftContract.deployed()
   console.log('Contract deployed to:', nftContract.address)
+  return nftContract
 }
 
-const runMain = async () => {
+async function runMain() {
   try {
     await main()
     process.exit(0)
