9343e92a921a24c49dca60b7b4958d71c2120c83
yyokii <yyokii.h@gmail.com>
2022-10-26 00:59:24 +0900

    Revert "♻️  Refactor folder structure"
    This reverts commit 9aa09a1e7e0a275dcda33639bdacaef61df3ab1a.

---
+++ grit-nfts/contracts/GritNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import 'hardhat/console.sol';
+
+contract GritNFT {
+  constructor() {
+    console.log('This is my NFT contract.');
+  }
+}
