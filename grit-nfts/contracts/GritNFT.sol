fa2519f74b831d50b35336344c5db18112a0cf0b
yyokii <yyokii.h@gmail.com>
2022-10-29 21:13:36 +0900

    🩹 Fix to set timestamp

--- grit-nfts/contracts/GritNFT.sol
+++ grit-nfts/contracts/GritNFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
+import "@openzeppelin/contracts/utils/Strings.sol";
 import 'hardhat/console.sol';
 import {Base64} from './libraries/Base64.sol';
 
@@ -26,19 +27,26 @@ contract GritNFT is ERC721URIStorage {
     uint256 newItemId = _tokenIds.current();
 
     string memory tokenSvg = string(abi.encodePacked(baseSvg, 'demo', '</text></svg>'));
+    uint createdAt = block.timestamp;
+    uint dueDate = block.timestamp; // TODO
+
     string memory json = Base64.encode(
       bytes(
         string(
           abi.encodePacked(
-            '{"name": "',
-            'this is demo',
-            '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-            Base64.encode(bytes(tokenSvg)),
-            '"}'
+            '{"name": "this is demo",',
+            '"description": "A highly acclaimed collection of squares.",',
+            '"image": "data:image/svg+xml;base64,', Base64.encode(bytes(tokenSvg)), '",', 
+            '"attributes": [',
+            '{"display_type": "date", "trait_type": "created_at", "value": "', Strings.toString(createdAt),'"},', 
+            '{"display_type": "date", "trait_type": "due_date", "value": "', Strings.toString(dueDate),'"}', 
+            ']',
+            '}'
           )
         )
       )
     );
+    console.log('json: %s', json);
     string memory tokenUri = string(abi.encodePacked('data:application/json;base64,', json));
 
     _safeMint(msg.sender, newItemId);
