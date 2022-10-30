d678a4857f27d4f2c355ccdc739ced7139b88ec2
yyokii <yyokii.h@gmail.com>
2022-10-30 22:35:31 +0900

    🎨 Add base UI

---
+++ grit-nft-app/src/components/FeaturesList.tsx
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Container,
+  Heading,
+  SimpleGrid,
+  Icon,
+  Text,
+  Stack,
+  HStack,
+  VStack,
+} from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons'
+
+// Replace test data with your own
+const features = [
+  {
+    id: 1,
+    title: 'aaaaa',
+    text: 'aaaaa',
+  },
+  {
+    id: 2,
+    title: 'bbbbb',
+    text: 'bbbbb',
+  },
+  {
+    id: 3,
+    title: 'ccccc',
+    text: 'ccccc',
+  },
+]
+
+export default function FeaturesList() {
+  return (
+    <Box p={4}>
+      <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'}>
+        <Heading fontSize={'3xl'}>Features</Heading>
+      </Stack>
+
+      <Container maxW={'6xl'} mt={10}>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+          {features.map((feature) => (
+            <HStack key={feature.id} align={'top'}>
+              <Box color={'green.400'} px={2}>
+                <Icon as={CheckIcon} />
+              </Box>
+              <VStack align={'start'}>
+                <Text fontWeight={600}>{feature.title}</Text>
+                <Text color={'gray.600'}>{feature.text}</Text>
+              </VStack>
+            </HStack>
+          ))}
+        </SimpleGrid>
+      </Container>
+    </Box>
+  )
+}
--- grit-nft-app/src/pages/index.tsx
+++ grit-nft-app/src/pages/index.tsx
@@ -1,9 +1,15 @@
-import { Box, chakra } from '@chakra-ui/react'
+import { Box, VStack } from '@chakra-ui/react'
+import FeaturesList from '../components/FeaturesList'
+import Hero from '../components/Hero'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <Box>
-      <chakra.h1 color='tomato'>Hello World!</chakra.h1>
-    </Box>
+    <Layout>
+      <VStack spacing={4} align='center'>
+        <Hero />
+        <FeaturesList />
+      </VStack>
+    </Layout>
   )
 }
