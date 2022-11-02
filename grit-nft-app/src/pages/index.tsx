d36fa4d0d24bcfce4af2b7516a82c6c9c76bd309
yyokii <yyokii.h@gmail.com>
2022-11-03 01:29:33 +0900

    🚧 Use web3 storage

--- grit-nft-app/src/pages/index.tsx
+++ grit-nft-app/src/pages/index.tsx
@@ -5,6 +5,8 @@ import Layout from '../components/Layout'
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import NormalButton from '../components/common/NormalButton'
+import { Web3Storage } from 'web3.storage'
+import GritNFT from '../grit-nfts.json'
 
 interface Window {
   ethereum?: ethers.providers.ExternalProvider
@@ -12,6 +14,12 @@ interface Window {
 
 export default function Home() {
   const [account, setAccount] = useState<string>(null)
+
+  // NFT metadata
+  const [nftName, setNftName] = useState<string>(null)
+  const [nftDescription, setNftDescription] = useState<string>(null)
+  const [nftDueDate, setNftDueDate] = useState<number>(null)
+
   const [isSending, setIsSending] = useState(false)
 
   useEffect(() => {
@@ -72,6 +80,52 @@ export default function Home() {
     } catch (error) {
       setIsSending(false)
       console.log(error)
+    }
+  }
+
+  const requestContractToMint = async (ipfs: string) => {
+    // TODO: アドレスを修正する
+    setIsSending(true)
+    try {
+      const { ethereum } = window as Window
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(
+          '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+          GritNFT.abi,
+          signer,
+        )
+        const transaction = await contract.makeNFT(nftName, nftDescription, ipfs, nftDueDate)
+        await transaction.wait()
+        console.log(`${ipfs} minted!`)
+        // TOOD: uplodat metadata to firestore for caching
+        setIsSending(false)
+      } else {
+        console.log('Ethereum object not found')
+      }
+    } catch (error) {
+      setIsSending(false)
+      console.log(error)
+    }
+  }
+
+  const imageToNFT = async (e) => {
+    const client = new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3_STORAGE_API_KEY })
+    const image = e.target
+    console.log(image)
+
+    const rootCid = await client.put(image.files, {
+      name: 'nft grit image',
+      maxRetries: 3,
+    })
+    const res = await client.get(rootCid)
+    const files = await res.files()
+
+    // TODO: 先頭のファイルのみを取得でいい気がする
+    for (const file of files) {
+      console.log('file.cid:', file.cid)
+      requestContractToMint(file.cid)
     }
   }
 
